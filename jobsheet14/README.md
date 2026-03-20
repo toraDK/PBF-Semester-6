a4638a8530254ad6215cc69fe35df69c85c30855
toraDK <toradigda@gmail.com>
2026-03-21 01:20:35 +0700

    Jobsheet14-Langkah5

--- jobsheet14/README.md
+++ jobsheet14/README.md
@@ -16,3 +16,15 @@ copy hasil generate nilai random base64 ke file .env
 Bagian 4 – Tambahkan SessionProvider
 memodifikasi file _app.tsx
 ![alt text](gambar_dokumentasi/6.png)
+
+Bagian 5 – Tambahkan Tombol Login & Logout
+Edit file index.tsx pada folder component/navbar
+![alt text](gambar_dokumentasi/7.png)
+edit file navbar.module.css
+![alt text](gambar_dokumentasi/8.png)
+Hasil :
+![alt](gambar_dokumentasi/Hasilnext-authCredentials.gif)
+edit file index.tsx untuk mendapatkan session nya
+![alt text](gambar_dokumentasi/9.png)
+Hasil :
+![alt](gambar_dokumentasi/Hasilnext-authCredentials2.gif)
--- jobsheet14/my-app/src/components/layouts/navbar/index.tsx
+++ jobsheet14/my-app/src/components/layouts/navbar/index.tsx
@@ -1,11 +1,21 @@
-import styles from "./navbar.module.css";
+import styles from './navbar.module.css';
+import { signIn, signOut, useSession } from "next-auth/react"
 
 const Navbar = () => {
+  const {data} = useSession();
+
   return (
     <div className={styles.navbar}>
-        <div className="big">Navbar Component</div>
+      <div className="big">
+        Navbar
+      </div>
+      {data ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
     </div>
-    )
-}
+  );
+};
 
 export default Navbar;
