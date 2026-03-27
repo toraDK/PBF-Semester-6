a95c6661d77c643ab3cb2583a0243978079ac90c
toraDK <toradigda@gmail.com>
2026-03-27 19:58:56 +0700

    jobsheet17-Langkah1

---
+++ jobsheet17/README.md
@@ -0,0 +1,3 @@
+### Konfigurasi Google OAuth
+Langkah 1 – Masuk ke Google Cloud Console Buka:<br>
+![alt text](gambar_dokumentasi/1.png)<br><br>
---
+++ jobsheet17/my-app/src/views/auth/register/index.tsx
@@ -0,0 +1,111 @@
+import Link from "next/link";
+import style from "./register.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const TampilanRegister = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError("");
+
+    const form = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const fullname = formData.get("Fullname") as string;
+    const password = formData.get("Password") as string;
+
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, fullname, password }),
+    });
+
+    //mengambil data dari response
+    const result = await response.json();
+
+    if (response.status === 200) {
+      form.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError(result.message); 
+    }
+  };
+
+  return (
+    <div className={style.register}>
+      {error && <p className={style.register__error}>{error}</p>}
+      <h1 className={style.register__title}>Halaman Register</h1>
+      <div className={style.register__form}>
+        <form onSubmit={handleSubmit}>
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="email"
+              className={style.register__form__item__label}
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              className={style.register__form__item__input}
+            />
+          </div>
+
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="Fullname"
+              className={style.register__form__item__label}
+            >
+              Fullname
+            </label>
+            <input
+              type="text"
+              id="Fullname"
+              name="Fullname"
+              placeholder="Fullname"
+              className={style.register__form__item__input}
+            />
+          </div>
+
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="Password"
+              className={style.register__form__item__label}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="Password"
+              name="Password"
+              placeholder="Password"
+              className={style.register__form__item__input}
+            />
+          </div>
+          
+          <button 
+            type="submit" 
+            className={style.register__form__item__button}
+            disabled={isLoading}>
+            {isLoading ? "Loading..." : "Register"}
+          </button>
+        </form>
+        <br />
+        <p className={style.register__form__item__text}>
+          Sudah punya akun? <Link href="/auth/login">Ke Halaman Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TampilanRegister;  
